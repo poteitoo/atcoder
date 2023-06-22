48edb8673b887c7df7f18958ad296a12ef68f905
poteitoo <1105tigery@gmail.com>
2023-06-22 21:22:34 +0000

    update 1.69 version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 cargo install cargo-compete
 rustup default 1.42.0-aarch64-unknown-linux-gnu
 rustup component add rustfmt
-rustup default 1.69
+rustup default 1.69.0-aarch64-unknown-linux-gnu
 rustup component add rustfmt
