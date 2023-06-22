ee754d92e0e20c6dbdea89878c040d12b20871e0
poteitoo <1105tigery@gmail.com>
2023-06-22 00:20:53 +0000

    typical90-004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [[usize;m];n]
+    }
+    let mut rows = Vec::new();
+    let mut cols = Vec::new();
+
+    for row in a.iter() {
+        let row_sum: usize = row.iter().sum();
+        rows.push(row_sum);
+    }
+
+    for col in 0..m {
+        let col_sum: usize = a.iter().map(|row| row[col]).sum();
+        cols.push(col_sum);
+    }
+
+    for (i, row) in rows.iter().enumerate() {
+        let mut res = Vec::new();
+        for (j, col) in cols.iter().enumerate() {
+            res.push(format!("{}", row + col - a[i][j]));
+        }
+        println!("{}", res.join(" "));
+    }
 }
