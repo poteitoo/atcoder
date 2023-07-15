13bbb1dbaeb5d9e5e30aa774f9c8ddfb9b312d73
poteitoo <1105tigery@gmail.com>
2023-07-15 11:57:33 +0000

    solve

--- arc164/src/bin/a.rs
+++ arc164/src/bin/a.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
+fn base3(mut n: i64) -> i64 {
+    let mut x = 0;
+    while 0 < n {
+        x += n % 3;
+        n /= 3;
+    }
+    x
+}
+
 fn main() {
-    todo!();
+    input!(n: usize, nk: [(i64, i64); n]);
+    for (n, k) in nk {
+        let x = base3(n);
+        if (n - k) % 2 == 0 && x <= k {
+            println!("{}, {}", n, x);
+            println!("Yes")
+        } else {
+            println!("{}, {}", n, x);
+            println!("No")
+        }
+    }
 }
