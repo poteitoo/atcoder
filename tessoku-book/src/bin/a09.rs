48c75a60ccda926607df07e8e10526d887a31b46
poteitoo <1105tigery@gmail.com>
2023-07-15 13:51:20 +0000

    solve

--- tessoku-book/src/bin/a09.rs
+++ tessoku-book/src/bin/a09.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        n: usize,
+        abcd: [(usize,usize,usize,usize);n]
+    }
+
+    let mut dp = vec![vec![0_isize; w + 1]; h + 1];
+    for &(a, b, c, d) in abcd.iter() {
+        dp[a][b] += 1;
+        dp[c + 1][b] -= 1;
+        dp[a][d + 1] -= 1;
+        dp[c + 1][d + 1] += 1;
+    }
+
+    for i in 0..h {
+        let mut v = 0;
+        let row = (0..w)
+            .map(|j| {
+                dp[i + 1][j + 1] += dp[i][j + 1];
+                v += dp[i + 1][j + 1];
+                v
+            })
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+        println!("{}", row);
+    }
 }
--- tessoku-book/src/bin/a10.rs
+++ tessoku-book/src/bin/a10.rs
@@ -1,3 +1,22 @@
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+        d: usize,
+        lr: [(Usize1, Usize1); d],
+    }
+    let mut max_l = vec![0; n + 1];
+    let mut max_r = vec![0; n + 1];
+
+    for i in 0..n {
+        let rev_i = n - i - 1;
+        max_l[i + 1] = max_l[i].max(a[i]);
+        max_r[rev_i] = max_r[rev_i + 1].max(a[rev_i]);
+    }
+    for (l, r) in lr {
+        println!("{}", max_l[l].max(max_r[r + 1]));
+    }
 }
