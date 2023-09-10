b2be7ce7c3470a6d9029849f7a7d2fef41a7fafc
poteitoo <1105tigery@gmail.com>
2023-09-10 03:46:30 +0000

    solve

--- tessoku-book/src/bin/b13.rs
+++ tessoku-book/src/bin/b13.rs
@@ -1,3 +1,23 @@
+use itertools::Itertools;
+use itertools_num::ItertoolsNum;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n]
+    }
+
+    let a = a.iter().cumsum::<usize>().collect_vec();
+    println!("{:?}", a);
+
+    let (mut l, mut ans) = (0, 0);
+    for r in 0..n - 1 {
+        while l <= r && a[r + 1] - a[l] > k {
+            l += 1;
+        }
+        ans += r - l + 1;
+    }
+    println!("{}", ans)
 }
