86430436f15f6fa55dcc1ad186c85903013bdfe0
poteitoo <1105tigery@gmail.com>
2023-09-16 13:47:11 +0000

    solve

--- abc320/src/bin/b.rs
+++ abc320/src/bin/b.rs
@@ -1,3 +1,39 @@
+use proconio::input;
+
+fn longest_palindrome(s: &str) -> usize {
+    let n = s.len();
+    if n == 0 {
+        return 0;
+    }
+
+    let mut dp = vec![vec![false; n]; n];
+    let mut left = 0;
+    let mut right = 0;
+
+    for i in (0..n).rev() {
+        dp[i][i] = true;
+        for j in i + 1..n {
+            if s.as_bytes()[i] == s.as_bytes()[j] {
+                if j - i == 1 {
+                    dp[i][j] = true;
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+                if dp[i][j] && j - i > right - left {
+                    left = i;
+                    right = j;
+                }
+            }
+        }
+    }
+
+    right - left
+}
+
 fn main() {
-    todo!();
+    input! {
+        s: String
+    }
+    let ans = longest_palindrome(s.as_str());
+    println!("{}", ans + 1)
 }
--- abc320/src/bin/c.rs
+++ abc320/src/bin/c.rs
@@ -1,3 +1,48 @@
+use std::collections::HashSet;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        m: usize,
+        s1: String,
+        s2: String,
+        s3: String,
+    }
+    let mut arr = vec![0; 10];
+    let mut s1_map: HashSet<usize> = HashSet::new();
+    let mut s2_map: HashSet<usize> = HashSet::new();
+    let mut s3_map: HashSet<usize> = HashSet::new();
+    for i in 0..m {
+        if let Ok(s) = s1.chars().nth(i).unwrap().to_string().parse::<usize>() {
+            if s1_map.insert(s) {
+                arr[s] += 1;
+                if arr[s] == 3 {
+                    println!("{}", i);
+                    return;
+                }
+            }
+        }
+
+        if let Ok(s) = s2.chars().nth(i).unwrap().to_string().parse::<usize>() {
+            if s2_map.insert(s) {
+                arr[s] += 1;
+            }
+            if arr[s] == 3 {
+                println!("{}", i);
+                return;
+            }
+        }
+
+        if let Ok(s) = s3.chars().nth(i).unwrap().to_string().parse::<usize>() {
+            if s3_map.insert(s) {
+                arr[s] += 1;
+            }
+            if arr[s] == 3 {
+                println!("{}", i);
+                return;
+            }
+        }
+    }
+    println!("-1");
 }
