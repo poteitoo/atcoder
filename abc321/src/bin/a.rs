3f53ec67832bc5a8efd0aaccc6ecf28c10281d35
poteitoo <1105tigery@gmail.com>
2023-09-23 14:59:25 +0000

    solve

--- abc321/src/bin/a.rs
+++ abc321/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        nn: Chars
+    }
+    for i in 1..nn.len() {
+        let a = nn[i - 1].to_digit(10).unwrap();
+        let b = nn[i].to_digit(10).unwrap();
+
+        if a <= b {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: isize,
+        a: [isize; n-1]
+    }
+    let n = n - 1;
+    let mut a = a;
+    a.sort_unstable();
+    let c = x - a[1..n - 1].into_iter().sum::<isize>();
+
+    if c > a[n - 1] {
+        println!("-1");
+        return;
+    }
+    if c <= a[0] {
+        println!("0");
+        return;
+    }
+    println!("{}", c);
 }
