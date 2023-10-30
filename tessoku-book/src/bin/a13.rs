18edc310ec0232d8d8841f3b582ef5a6b74e5a13
poteitoo <1105tigery@gmail.com>
2023-10-30 05:53:44 +0000

    resolve a13

--- tessoku-book/src/bin/a13.rs
+++ tessoku-book/src/bin/a13.rs
@@ -2,26 +2,50 @@
 
 use proconio::input;
 
-fn main() {
+// fn shakutori() {
+//     input! {
+//         n: usize,
+//         k: usize,
+//         a: [usize; n]
+//     }
+//     let mut r = vec![0; n];
+
+//     for i in 0..n - 1 {
+//         if i == 0 {
+//             r[i] = 0;
+//         } else {
+//             r[i] = r[i - 1];
+//         }
+
+//         while r[i] < n && a[r[i]] - a[i] <= k {
+//             r[i] += 1;
+//         }
+//     }
+
+//     let ans: usize = r.iter().enumerate().fold(0, |p, (i, x)| p + *x - i);
+//     println!("{}", ans);
+// }
+
+// original
+fn original() {
     input! {
         n: usize,
         k: usize,
         a: [usize; n]
     }
-    let mut r = vec![0; n];
+    let mut r = 0;
+    let mut ans = 0;
 
     for i in 0..n - 1 {
-        if i == 0 {
-            r[i] = 0;
-        } else {
-            r[i] = r[i - 1];
+        while r < n && a[r] - a[i] <= k {
+            r += 1;
         }
-
-        while r[i] < n && a[r[i]] - a[i] <= k {
-            r[i] += 1;
-        }
+        ans += r - i - 1;
     }
 
-    let ans: usize = r.iter().enumerate().fold(0, |p, (i, x)| p + *x - i);
     println!("{}", ans);
+}
+
+fn main() {
+    original();
 }
