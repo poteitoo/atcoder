6db873dbf9568e5422056e927c16def0e871586f
poteitoo <1105tigery@gmail.com>
2023-12-09 16:01:54 +0000

    solve

--- arc169/src/bin/a.rs
+++ arc169/src/bin/a.rs
@@ -7,5 +7,19 @@ fn main() {
         p: [usize; n-1]
     }
 
-    println!("{:?}", p)
+    let mut distance = vec![0; n];
+    for i in 2..=n {
+        distance[i - 1] = distance[p[i - 2] - 1] + 1;
+    }
+    let mut sums = vec![0; n];
+    for i in 0..n {
+        sums[distance[i]] += a[i];
+    }
+    for sum in sums.iter().rev() {
+        if *sum != 0 {
+            println!("{}", if *sum > 0 { "+" } else { "-" });
+            return;
+        }
+    }
+    println!("0");
 }
